889248f701c88b9210e16cca04eb62bd0839ec89
schrk06 <123100767+schrk06@users.noreply.github.com>
2025-05-10 01:32:24 +0200

    initialisation du projet

---
+++ src/main/java/salim/test/todo_list/Controller/TaskController.java
@@ -0,0 +1,19 @@
+package salim.test.todo_list.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ch.qos.logback.core.net.SyslogOutputStream;
+
+@RestController
+@RequestMapping(path = "tasks")
+public class TaskController {
+
+    @GetMapping(path = "string")
+    public String geString(){
+        System.out.println("salut");
+        return "Salut";
+    }
+ 
+}
