c41d8c5a62c05aa0023d6babc02c1d6ad87e105f
schrk06 <123100767+schrk06@users.noreply.github.com>
2025-05-12 01:45:58 +0200

    fixing errors

--- src/main/java/salim/test/todo_list/Model/Task.java
+++ src/main/java/salim/test/todo_list/Model/Task.java
@@ -7,7 +7,7 @@ import java.time.LocalDateTime;
 public class Task {
 
     @Id
-    @GeneratedValue(strategy = GenerationType. IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String titre;
     private String description;
